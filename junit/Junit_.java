58bf1d0765260b81639b159cea576dc4bf01f6c3
懒羊羊 <2251487365@qq.com>
2026-07-13 11:14:51 +0800

    Add files via upload

---
+++ junit/Junit_.java
@@ -0,0 +1,19 @@
+package com.junit;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author lanyangyang
+ */
+public class Junit_ {
+    static void main(String[] args) {
+
+    }
+    @Test
+    public void m1(){
+        System.out.println("m1被调用");
+    }
+    public void m2(){
+        System.out.println("m2被调用");
+    }
+}
